d6afdcfd79e6436b216b4522b404946309308e71
Helen <sang.s.yi@gmail.com>
2021-04-08 00:23:58 -0700

    router

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+// Set up GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
+
+// // this code
+// router.route('/').get(getCallbackFunction).post(postCallbackFunction);
+
+// // is this same as this
+// router.get('/', getCallbackFunction);
+// router.post('/' postCallbackFunction);
